467e11805a51deaac42df16ac65b6c87c5c68fc3
nikolajanembo <nikolajanembo@gmail.com>
2025-12-12 12:48:29 +0100

    Added wave numbers and start wave controll

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -1,29 +1,73 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
-    [SerializeField] private int spawnAmount;
     [SerializeField]private Vector2 spawndelayRange;
     [SerializeField] private float sphereSpawnRange;
-    private int enemiesSpawned = 0;
+    [SerializeField] private Button spawnButton;
+    [SerializeField] private TextMeshProUGUI waveNumberText;
+    [SerializeField] private List<int> waves;
+    [SerializeField] private float waveLenght;
+    [SerializeField] private TextMeshProUGUI waveTimerText;
     
-    private void Start()
+    private bool isTimerActive;
+    private float waveTimer;
+    private int currentWave;
+
+    private void Awake()
     {
-        StartCoroutine(SpawnEnemies());
+        waveNumberText.text = "Wave: " + currentWave + "/" + waves.Count.ToString();
+        waveTimerText.text = " ";
+    }
+
+    private void OnEnable()
+    {
+        spawnButton.onClick.AddListener(StartSpawning);
+    }
+
+    private void OnDisable()
+    {
+        spawnButton.onClick.RemoveListener(StartSpawning);
     }
 
     private IEnumerator SpawnEnemies()
     {
-        for (enemiesSpawned = 0; enemiesSpawned < spawnAmount; enemiesSpawned++)
+        isTimerActive = false;
+        spawnButton.gameObject.SetActive(false);
+        waveNumberText.text = "Wave: " + (currentWave + 1) + "/" + waves.Count.ToString();
+        for (int enemiesSpawned = 0; enemiesSpawned < waves[currentWave]; enemiesSpawned++)
         {
             yield return new WaitForSeconds(Random.Range(spawndelayRange.x, spawndelayRange.y));
             Instantiate(enemy,transform.position  + Random.insideUnitSphere*sphereSpawnRange,Quaternion.identity);
+        }
+        spawnButton.gameObject.SetActive(true);
+        currentWave++;
+        StartCoroutine(WaveTimer());
+    }
+
+    private IEnumerator WaveTimer()
+    {
+        isTimerActive = true;
+        waveTimer = Time.time + waveLenght;
+        while (isTimerActive && waveTimer > Time.time)
+        {
+            yield return null;
+            waveTimerText.text = "Next wave in: " + (waveTimer - Time.time).ToString("F0"); 
+        }
+        waveTimerText.text = " ";
+
+        if (isTimerActive == true)
+        {
+            StartSpawning();
         }
     }
 
@@ -31,5 +75,13 @@ public class EnemySpawn : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, sphereSpawnRange);
+    }
+
+    private void StartSpawning()
+    {
+        if (currentWave < waves.Count)
+        {
+            StartCoroutine(SpawnEnemies());
+        }
     }
 }
